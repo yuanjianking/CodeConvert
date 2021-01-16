888f9e7e17a52312c4da3452afd4adbcb5beac2b
yuanjianking <yuanjianking@hotmail.com>
2021-01-16 21:05:49 +0800

    新建

---
+++ Core/Convert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeConvert.Core
+{
+    class Convert
+    {
+        public static void Run()
+        {
+            // 取得参数
+
+            // 读取源文件
+            
+            // 创建目标文件夹
+
+            // 判断输入类型和输出类型
+
+            // 加载输入类型的配置 语言版本控制   
+
+            // 加载输出类型的配置 语言版本控制
+
+            // 创建输入输出语法规则表
+
+            // 创建符号表
+
+            // 词法分析
+
+            // 语法分析
+
+            // 语义分析
+
+            // 生成中间结构
+
+            // 转换目标代码
+
+            // 优化代码
+
+            // 输出目标代码
+
+
+            //异常输出
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using CodeConvert.Core;
+using System;
+using Convert = CodeConvert.Core.Convert;
+
+namespace CodeConvert
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            if (args != null)
+            {
+                int argsLength = args.Length;
+                for (int i = 0; i < argsLength; i += 2)
+                {
+                    string arg = args[i];
+                    char[] c = arg.ToCharArray();
+                    if ('-'.Equals(c[0]))
+                    {
+                        switch (c[1])
+                        {
+                            case 's':
+                                if(!SetSourcePath(args[i + 1])) return;
+                                break;
+                            case 'd':
+                                if(!SetDestinationPath(args[i + 1])) return;
+                                break;
+                            case 'i':
+                                if(!SetInputType(args[i + 1])) return;
+                                break;
+                            case 'o':
+                                if(!SetOutputType(args[i + 1])) return;
+                                break;
+                            default:
+                                Console.WriteLine("Wrong Parameters.No Prefix");
+                                return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong Prefix.");
+                        return; 
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Parameters.");
+                return;
+            }
+            Convert.Run();
+            Console.WriteLine("Complete!");
+        }
+
+        private static bool CheckArg(string arg)
+        {
+            bool res = true;
+            if (string.IsNullOrEmpty(arg))
+            {
+                Console.WriteLine("Wrong Prefix.No Arg");
+                res = false;
+            }
+            else if ('-'.Equals(arg.ToCharArray()[0]))
+            {
+                Console.WriteLine("Wrong Prefix.No Arg");
+                res = false;
+            }
+            return res;
+        }
+
+
+        private static bool SetSourcePath(string path)
+        {
+            bool res = true;
+            if (CheckArg(path))
+            {
+                Global.CreateInstance().SourcePath = path;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+        private static bool SetDestinationPath(string path)
+        {
+            bool res = true;
+            if (CheckArg(path))
+            {
+                Global.CreateInstance().DestinationPath = path;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+        private static bool SetInputType(string inType)
+        {
+            bool res = true;
+            if (CheckArg(inType))
+            {
+                Global.CreateInstance().InputType = inType;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+        private static bool SetOutputType(string outType)
+        {
+            bool res = true;
+            if (CheckArg(outType))
+            {
+                Global.CreateInstance().OutputType = outType;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}

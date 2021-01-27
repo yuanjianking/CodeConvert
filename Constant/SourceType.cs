c1e323b20012ebd261686ee5d7ffd7b3010ea9a1
yuanjianking <yuanjianking@hotmail.com>
2021-01-27 13:37:46 +0800

    update

---
+++ Constant/SourceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeConvert.Constant
+{
+    enum SourceType : ushort
+    {
+        C = 0,
+        Cpp = 1,
+        Cs = 2,
+        Java = 3,
+        JS = 4,
+        Undefine = 99
+    }
+}
---
+++ Core/SourceManager.cs
@@ -0,0 +1,40 @@
+﻿using CodeConvert.Constant;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeConvert.Core
+{
+    class SourceManager
+    {
+        private string v = "";
+        private SourceType type = SourceType.Undefine;
+
+        public SourceManager(string version, SourceType type)
+        {
+            this.v = version;
+            this.type = type;        
+        }
+
+        public void LoadIn()
+        { 
+            // 先判断有咩有该语言的定义资源
+
+            // 在判断有没有该语言的转换版本
+
+            // 再判断有没有该语言的输入配置
+        
+        }
+
+
+        public void LoadOut()
+        {
+            // 先判断有咩有该语言的定义资源
+
+            // 在判断有没有该语言的转换版本
+
+            // 再判断有没有该语言的输出配置
+
+        }
+    }
+}
